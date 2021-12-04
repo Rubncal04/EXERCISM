e4e1be4ba36dc2329847c74d41aa96a2c46ec863
Ruben <rubncal.9501@gmail.com>
2021-12-03 22:36:15 -0500

    implemented case method and constant

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -2,23 +2,32 @@
 
 # the production of an assembly line in a car factory.
 class AssemblyLine
+  HOURLY_YIELD = 221
+
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
-    if @speed <= 4
-      @speed * 221 * 1.0
-    elsif @speed <= 8
-      @speed * 221 * 0.90
-    elsif @speed == 9
-      @speed * 221 * 0.80
-    else
-      @speed * 221 * 0.77
-    end
+    @speed * HOURLY_YIELD * success_rate
   end
 
   def working_items_per_minute
     (production_rate_per_hour / 60).to_i
+  end
+
+  private
+
+  def success_rate
+    case @speed
+    when 1..4
+      1.0
+    when 5..8
+      0.90
+    when 9
+      0.80
+    when 10
+      0.77
+    end
   end
 end
