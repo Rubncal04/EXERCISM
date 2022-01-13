b0b0de54e550a8c555bd10e19fdfe3c90ba025db
Ruben <rubncal.9501@gmail.com>
2022-01-13 12:32:44 -0500

    better solution for acronym using unicode in regex

--- python/guidos-gorgeous-lasagna/lasagna_test.py
+++ python/guidos-gorgeous-lasagna/lasagna_test.py
@@ -1,6 +1,6 @@
 import unittest
 import pytest
-import sys
+# import sys
 
 try:
     from lasagna import (
--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -3,6 +3,6 @@
 # This class converts a phrase to its acronym
 class Acronym
   def self.abbreviate(phrase)
-    phrase.scan(/\b(\w)/).join('').upcase
+    phrase.scan(/\b[[:alpha:]]/).join('').upcase
   end
 end
