f043067a625b956a509df34eb65e2f7ce52a7ba8
Ruben Gomez <rubncal.9501@gmail.com>
2022-12-07 12:21:47 -0500

    Better solution for isogram

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -2,18 +2,9 @@
 
 # Class isogram with isogram? method that returns boolean if word is or isn't an isogram
 class Isogram
-  def self.isogram?(input)
-    input = input.split('')
-    count = 0
-    letter_group = []
-    input.each do |i|
-      i = i.downcase
-      if !letter_group.include?(i) || i == ' ' || i == '-'
-        letter_group << i
-      else
-        count += 1
-      end
-    end
-    !count.positive? || letter_group.empty?
+  def self.isogram?(word)
+    word = word.downcase.scan(/\w/)
+    no_duplicates = word.uniq
+    word.length == no_duplicates.length
   end
 end
