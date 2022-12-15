ce1cb903d355d16f37eaa23df5ee9c14de78850a
Ruben Gomez <rubncal.9501@gmail.com>
2022-12-15 16:55:15 -0500

    Remove + 1 inside count loop

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -5,6 +5,6 @@ class Hamming
   def self.compute(strand_one, strand_two)
     raise ArgumentError if strand_one.length != strand_two.length
 
-    (0..strand_one.size).count { |i| + 1 if strand_one[i] != strand_two[i] }
+    (0..strand_one.size).count { |i| strand_one[i] != strand_two[i] }
   end
 end
