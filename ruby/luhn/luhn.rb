43af484ccdbcfeca978e0dbfb53eaad7a98a3cd7
Ruben Gomez <rubncal.9501@gmail.com>
2022-12-20 11:05:22 -0500

    Better solution for luhn

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -2,13 +2,28 @@
 
 # This class returns a boolean value
 class Luhn
-  def self.valid?(number)
-    number = number.gsub(' ', '').chars.reverse
-    return false unless number.length > 1
-    return false if number.any?(/[[:alpha:]]/)
+  attr_reader :number
 
+  def initialize(number)
+    @number = number
+  end
+
+  def self.valid?(number)
+    number = new(number)
+    return false unless number.convert_string_to_array.length > 1
+
+    return false if number.convert_string_to_array.any?(/[[:alpha:]]/)
+
+    number.divisible_by_ten
+  end
+
+  def convert_string_to_array
+    @number.gsub(' ', '').chars.reverse
+  end
+
+  def divisible_by_ten
     luhn = []
-    number.each_with_index do |n, i|
+    convert_string_to_array.each_with_index do |n, i|
       n = n.to_i
       n *= 2 if i.to_i.odd?
       n -= 9 if n > 9
