f2409aa350fc55f4be4476f1fab2b77fdbc83a52
Ruben Gomez <rubncal.9501@gmail.com>
2024-05-23 16:46:51 +0000

    Better solution

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -6,8 +6,8 @@ import "fmt"
 
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-	if len(name) > 0 {
-		return fmt.Sprintf("One for %s, one for me.", name)
+	if name == "" {
+		return "One for you, one for me."
 	}
-	return "One for you, one for me."
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
