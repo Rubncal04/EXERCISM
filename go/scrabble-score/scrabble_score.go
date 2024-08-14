88073e55567229e01d51e743462d65354764edd1
Ruben Gomez <rubncal.9501@gmail.com>
2024-08-14 10:02:52 -0500

    Better solution for scrabble-score

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,19 +1,30 @@
 package scrabble
 
-import "unicode"
-
-var words = map[rune]int{
-	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1,
-	'S': 1, 'T': 1, 'D': 2, 'G': 2, 'B': 3, 'C': 3, 'M': 3, 'P': 3,
-	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4, 'K': 5, 'J': 8, 'X': 8,
-	'Q': 10, 'Z': 10,
-}
+import (
+	"unicode"
+)
 
 func Score(word string) int {
 	var total int
 
 	for _, w := range word {
-		total += words[unicode.ToUpper(w)]
+		w = unicode.ToUpper(w)
+		switch w {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			total += 1
+		case 'D', 'G':
+			total += 2
+		case 'B', 'C', 'M', 'P':
+			total += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			total += 4
+		case 'K':
+			total += 5
+		case 'J', 'X':
+			total += 8
+		case 'Q', 'Z':
+			total += 10
+		}
 	}
 	return total
 }
