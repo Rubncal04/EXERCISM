5aaa71d165abfc7a4768aa7ec0298180f742137a
Ruben Gomez <rubncal.9501@gmail.com>
2024-08-23 14:13:08 -0500

    Better solution for isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,12 +1,10 @@
 package isogram
 
 import (
-	"strings"
 	"unicode"
 )
 
 func IsIsogram(word string) bool {
-	word = strings.TrimSpace(word)
 	letterGroup := make(map[rune]bool)
 
 	for _, w := range word {
